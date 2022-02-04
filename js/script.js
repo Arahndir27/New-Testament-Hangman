2684c83a517fbc4bf33e1288b85d91c4034f1724
Arahndir27 <espressler18325@gmail.com>
2022-02-04 16:26:48 -0700

    Added Vue

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const possibleWords = [
+    "Peter",
+    "Simon",
+    "Andrew",
+    "James",
+    "Zebedee",
+    "John",
+    "Philip",
+    "Bartholomew",
+    "Thomas",
+    "Matthew",
+    "James son of Alphaeus",
+    "Thaddaeus",
+    "Simon the Canaanite",
+    "Judas Iscariot",
+    "Apostle",
+    "Mark",
+    "Luke",
+    "The Acts of the Apostles",
+    "Pharisee",
+    "Sadducee",
+    "Temple",
+    "New Testament",
+    "Messiah",
+    "Jesus of Nazareth",
+    "Jerusalem",
+    "Mary",
+    "Joseph",
+    "Stable",
+    "Bethlehem",
+    "Galilee",
+    "Capernaum",
+    "Samaria",
+    "Widow of Nain",
+    "Lazarus",
+    "Mary Magdalene",
+    "Martha",
+    "He is Risen",
+    "Golgotha"
+]
+
+function reset() {}
+
+function guessLetter() {}
+
+function reset() {}
