ff74f68c0f38d7647834094f5593e4ad6d4d4125
Arahndir27 <espressler18325@gmail.com>
2022-04-08 17:54:29 -0600

    Fix hover on moblie

--- js/script.js
+++ js/script.js
@@ -231,7 +231,33 @@ var hangman = new Vue({
                     this.lettersInWord.add(myChar.toLowerCase());
                 }
             }
-        }
+        },
+        watchForHover() {
+            // lastTouchTime is used for ignoring emulated mousemove events
+            // that are fired after touchstart events. Since they're
+            // indistinguishable from real events, we use the fact that they're
+            // fired a few milliseconds after touchstart to filter them.
+            let lastTouchTime = 0
+
+            function enableHover() {
+                if (new Date() - lastTouchTime < 500) return
+                document.body.classList.add('hasHover')
+            }
+
+            function disableHover() {
+                document.body.classList.remove('hasHover')
+            }
+
+            function updateLastTouchTime() {
+                lastTouchTime = new Date()
+            }
+
+            document.addEventListener('touchstart', updateLastTouchTime, true)
+            document.addEventListener('touchstart', disableHover, true)
+            document.addEventListener('mousemove', enableHover, true)
+
+            enableHover()
+        },
     },
     computed: {
         //Returns a new array of only letters that have not been guessed
@@ -257,4 +283,7 @@ var hangman = new Vue({
         this.chooseWord();
         this.formatDisplayWord();
     },
+    mounted() {
+        this.watchForHover();
+    }
 })
